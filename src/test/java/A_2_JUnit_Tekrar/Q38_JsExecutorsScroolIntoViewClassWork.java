f3bdc17928f80764d7289c5c995d9ee30bf91054
İdris <idris.gemici61@gmail.com>
2022-10-03 22:06:26 +0300

    03.10.22

--- src/test/java/A_2_JUnit_Tekrar/Q38_JsExecutorsScroolIntoViewClassWork.java
+++ src/test/java/A_2_JUnit_Tekrar/Q38_JsExecutorsScroolIntoViewClassWork.java
@@ -1,15 +1,38 @@
 package A_2_JUnit_Tekrar;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBaseBeforeAfter;
+import utilities.TestBaseBeforeClassAfterClass;
 
-public class Q38_JsExecutorsScroolIntoViewClassWork extends TestBaseBeforeAfter {
+public class Q38_JsExecutorsScroolIntoViewClassWork extends TestBaseBeforeClassAfterClass {
     @Test
-    public void test() {
-        // 1- Yeni bir class olusturun :ScroolInto
-        // 2- hotelmycamp anasayfasina gidin
-        // 3- 2 farkli test method’u olusturarak actions classi ve JsE xecutor kullanarak asagidaki oda turlerinden
-        //    ust sira ortadaki odayi tiklayin
-        // 4- istediginiz oda inceleme sayfasi acildigini test edin
+    public void testJsExecuter() {
+        //  2 hotelmycamp anasayfasinagidin
+        driver.get("https://www.hotelmycamp.com/");
+
+        //  3 2farklitestmethod’uolusturarak actionsclassi ve JsExecutor kullanarak asagidaki odaturlerinden
+        //  ustsira ortadaki odayi tiklayin
+        WebElement room= driver.findElement(By.xpath("(//*[@class='btn-custom'])[2]"));
+        JavascriptExecutor jse= (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].scrollIntoView(true);",room);
+        jse.executeScript("arguments[0].click();",room);
+
+        //  4istediginiz oda inceleme sayfasi acildigini test edin
+        Assert.assertTrue(driver.findElement(By.xpath("(//*[text()='Mrs. Fredericka Kling'])[1]")).isDisplayed());
+
+    }
+    @Test
+    public void testActions()  {
+        driver.get("https://www.hotelmycamp.com/");
+        WebElement room= driver.findElement(By.xpath("(//*[@class='btn-custom'])[2]"));
+        Actions actions=new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
+        room.click();
     }
 }
